e668aa58deb63a143db3c0fc9c2f943abc0557c9
abenezerayalew <aben.man.unity@gmail.com>
2023-01-24 14:28:58 -0800

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,27 +9,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-        size_t step, i;
+	size_t step, i;
 
-        for (step = 0; step < size - 1; ++step)
-        {
-                int swapped = 0;
+	for (step = 0; step < size - 1; ++step)
+	{
+		int swapped = 0;
 
-                for (i = 0; i < size - step - 1; ++i)
-                {
-                        if (array[i] > array[i + 1])
-                        {
-                                int temp = array[i];
+		for (i = 0; i < size - step - 1; ++i)
+		{
+			if (array[i] > array[i + 1])
+			{
+				int temp = array[i];
 
-                                array[i] = array[i + 1];
-                                array[i + 1] = temp;
-                                swapped = 1;
-                                print_array(array, size);
-                        }
-                }
-                if (swapped == 0)
-                {
-                        break;
-                }
-        }
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+		{
+			break;
+		}
+	}
 }
