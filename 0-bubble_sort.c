550d44d4de5c61d817c2152c6e2af7f809a9c424
Abenezer Belete <44866028+abenezerayalew@users.noreply.github.com>
2023-01-25 00:41:58 +0300

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,4 +3,17 @@
 void bubble_sort(int *array, size_t size)
 {
 
+  for (int repeat = 0; repeat < size - 1; ++repeat) {
+      
+    for (int i = 0; i < size - repeat - 1; ++i) {
+      
+      if (array[i] > array[i + 1]) {
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        print_array(array, size);
+      }
+    }
+ }
+
 }
